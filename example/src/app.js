1e23e4a09d0463de3faf5dfb111aa45e507927d5
William Thivierge <w.thivierge@gmail.com>
2017-06-05 09:59:38 -0400

    Added tiny bit of doc in readme and example application

--- example/src/app.js
+++ example/src/app.js
@@ -1,39 +1,3 @@
-# feathers-horizon
-
-[![Build Status](https://travis-ci.org/madwill/feathers-horizon.svg?branch=master)](https://travis-ci.org/madwill/feathers-horizon)
-<a href="https://codeclimate.com/github/madwill/feathers-horizon"><img src="https://codeclimate.com/github/madwill/feathers-horizon/badges/gpa.svg" /></a>
-<a href="https://codeclimate.com/github/madwill/feathers-horizon/coverage"><img src="https://codeclimate.com/github/madwill/feathers-horizon/badges/coverage.svg" /></a>
-[![GitHub issues](https://img.shields.io/github/issues/madwill/feathers-horizon.svg?style=flat-square)](https://github.com/madwill/feathers-horizon/issues)
-[![Download Status](https://img.shields.io/npm/dm/feathers-horizon.svg?style=flat-square)](https://www.npmjs.com/package/feathers-horizon)
-
-> Horizon.js feathers plugin, this plugin will start an horizon server instance using provided options or defaults feathers config. 
-
-##Goals
-
-Plugins intent to add missing horizon.js functionality by leveraging the excellent featherjs for backend. It uses feathers authentication for local strategy 
-
-This grants you the entire [featherjs ecosystem](https://github.com/feathersjs/feathers-docs/tree/master/ecosystem) around your horizon.js application
-
-## Installation
-
-```
-npm install feathers-horizon --save
-``` 
-
-## Create your app using feathers generators
-
-```
-feathers generate app
-feathers generate authentication
-npm install feathers-horizon --save
-```
-
-Then import and configure the plugin after [Authentication](https://github.com/feathersjs/feathers-authentication) plugin
-## Complete Example
-
-Here's an example of a Feathers server that uses `feathers-horizon`. 
-
-```js
 const path = require('path');
 const favicon = require('serve-favicon');
 const compress = require('compression');
@@ -50,9 +14,10 @@ const feathersHorizon = require('feathers-horizon');
 const middleware = require('./middleware');
 const services = require('./services');
 const appHooks = require('./app.hooks');
-
 const authentication = require('./authentication');
+
 const rethinkdb = require('./rethinkdb');
+
 const app = feathers();
 
 // Load app configuration
@@ -80,8 +45,6 @@ app.configure(feathersHorizon());
 // `middleware/index.js`) - always has to be last
 app.configure(middleware);
 app.hooks(appHooks);
-
-//Create a default users for our tests. 
 var User = {
 		email: 'admin@feathersjs.com',
 		password: 'admin',
@@ -95,10 +58,3 @@ app
 		})
 		.catch(console.error);
 module.exports = app;
-```
-
-## License
-
-Copyright (c) 2017
-
-Licensed under the [MIT license](LICENSE).
---
+++ example/src/services/index.js
@@ -0,0 +1,5 @@
+const users = require('./users/users.service.js');
+module.exports = function () {
+  const app = this; // eslint-disable-line no-unused-vars
+  app.configure(users);
+};
